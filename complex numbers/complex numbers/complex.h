b551283b4de3fe6aee1afbfab05250685121ec26
TEMYCH <artemchelebaev@gmail.com>
2020-04-19 19:45:42 +0300

    Updated

--- complex numbers/complex numbers/complex.h
+++ complex numbers/complex numbers/complex.h
@@ -10,11 +10,14 @@ public:
 	Complex(Complex& A);
 	void set(double a, double b);
 	Complex get();
+	
 	friend  Complex operator + (Complex& a, Complex& b);
-	friend Complex operator * (Complex& a, Complex& b);
 	friend Complex operator - (Complex& a, Complex& b);
+	friend Complex operator * (Complex& a, Complex& b);
 	friend Complex operator / (Complex& a, Complex& b);
+	
 	Complex& operator = (const Complex& a);
+	
 	friend bool operator == (Complex& a, Complex& b);
 	friend ostream& operator << (ostream& out, Complex& b);
 	friend istream& operator >> (istream& in, Complex& b);
